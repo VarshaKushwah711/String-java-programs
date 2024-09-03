6b3c154a780f463b3f76014555d48882a021b276
Varsha Kushwah <varshakushwah711@gmail.com>
2024-09-03 22:35:27 +0530

    Add files via upload

---
+++ STring21.java
@@ -0,0 +1,10 @@
+package Git_string;
+//Find the length of the string "Java Programming".
+public class STring21 {
+    public static void main(String[] args) {
+        
+        String str = "Java Programming";
+        System.out.println("length of the string Java Programming: " +str.length());
+    }
+    
+}
