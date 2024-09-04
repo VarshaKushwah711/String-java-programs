b7e054e03807cfaf4ca58644b63eeb2370ca29e6
Varsha Kushwah <varshakushwah711@gmail.com>
2024-09-04 13:16:46 +0530

    Add files via upload

---
+++ String22.java
@@ -0,0 +1,16 @@
+package Git_string;
+//3. Access the first character of the string "ChatGPT".
+//4. Access the last character of the string "Intelligence".
+public class String22 {
+    public static void main(String[] args) {
+
+        String str = "Java Programming";
+        String str1 = "Intelligence";
+
+        System.out.println("the first character of the string \"Java Programming\": " + str.charAt(0));
+        System.out.println("last character of the string \"Intelligence\": " + str1.charAt(str1.length()-1));
+        
+    }
+
+    
+}
